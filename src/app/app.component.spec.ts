a70473327c044aad79f74aae459c9773927d06f1
BwnacsHub <44450647+BwnacsHub@users.noreply.github.com>
2019-02-04 23:05:03 -0500

    CI Updates

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,5 @@
 import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { MaterialModule } from './material.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import {
   commonTestingModules,
   commonTestingProviders,
@@ -39,12 +34,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('lemon-mart');
-  });
-
-  it('should render title in a .mat-h2 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.mat-h2').textContent).toContain('LemonMart');
   });
 });
